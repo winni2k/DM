adaf864e86b7403fce741b56e67831defda0b6ef
Warren W. Kretzschmar <wkretzsch@gmail.com>
2013-02-26 12:15:08 +0000

    bugfix, PE was not exported in addrule

--- lib/DistributedMake/base.pm
+++ lib/DistributedMake/base.pm
@@ -1,5 +1,5 @@
 package DistributedMake::base;
-use version 0.77; our $VERSION = qv('0.1.008');
+use version 0.77; our $VERSION = qv('0.1.009');
 
 use 5.006;
 use strict;
@@ -14,7 +14,7 @@ DistributedMake::base - A perl module for running pipelines
 
 =head1 VERSION
 
-0.1.008
+0.1.009
 
 =head1 SYNOPSIS
 
@@ -151,6 +151,7 @@ sub addRule {
     my %bja = (
         'cluster'     => $self->{'cluster'},
         'queue'       => $self->{'queue'},
+        'PE'          => self->{'PE'},
         'memLimit'    => $self->{'memLimit'},
         'rerunnable'  => $self->{'rerunnable'},
         'name'        => $self->{'name'},
